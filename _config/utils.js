26e0fc0ebf442b0b27650de3a03883a21334d03b
byte <50130120+huckleton@users.noreply.github.com>
2024-04-02 05:49:29 -0400

    page date

--- _config/utils.js
+++ _config/utils.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter("keys", Object.keys);
 
 	const alphabetizePages = pages => {
@@ -10,5 +10,21 @@ module.exports = function(eleventyConfig) {
 		return pages;
 	};
 
+	const formatDate = (date) => {
+		if (!(date instanceof Date)) {
+			date = new Date(date);
+		}
+
+		const timezoneOffset = date.getTimezoneOffset();
+		date.setMinutes(date.getMinutes() + timezoneOffset);
+	
+		const day = date.getDate();
+		const suffixes = ["th", "st", "nd", "rd"];
+		const suffix = suffixes[(day - 20) % 10] || suffixes[day] || suffixes[0];
+		const formattedDate = day + suffix + " " + date.toLocaleString("en-US", { month: "long", year: "numeric" });
+		return formattedDate;
+	};
+
 	eleventyConfig.addFilter("alphabetizePages", alphabetizePages);
+	eleventyConfig.addFilter("formatDate", formatDate);
 };
